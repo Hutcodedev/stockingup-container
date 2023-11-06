4f355d81dc3da54fbf955615b0bd56e599eddba1
yamilzavala <zavalayamil9@gmail.com>
2023-11-05 21:47:38 -0300

    fetch files from user-mf

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederalPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 module.exports = {
     mode: 'development',
@@ -6,6 +7,12 @@ module.exports = {
         port: 8080
     },
     plugins: [
+        new ModuleFederalPlugin({
+            name: 'container',            
+            remotes: {
+                users: 'users@http://localhost:8081/remoteEntry.js'
+            }
+        }),
         new HtmlWebpackPlugin(
             {
                 template: './public/index.html'
